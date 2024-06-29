a050bd988e86704abd01a33b86de2474fbd3364a
Ryan Montgomery <coap.ryanmontgomery@gmail.com>
2024-06-28 21:41:51 -0600

    counted down w sleep
    counted down passed

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,15 @@
-# your code goes here!
+import time
+
+
+def countdown(number):
+    while number > 0:
+        print(f"{number} SECOND(S)!")
+        number -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(number):
+    while number > 0:
+        print(f"{number} SECOND(S)!")
+        number -= 1
+        time.sleep(1) # Sleep for 1 second
+    print("HAPPY NEW YEAR!")
